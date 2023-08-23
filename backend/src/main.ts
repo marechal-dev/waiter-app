cdccce120d9f8dd5131fe147b4bfade9c6f9357a
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-08-23 13:48:17 -0300

    feat: create server

---
+++ backend/src/main.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+import express from "express";
+
+const PORT = Number(process.env.PORT) ?? 3333;
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(PORT, () => console.log(`⚙ Server running at http://localhost:${PORT}`));
